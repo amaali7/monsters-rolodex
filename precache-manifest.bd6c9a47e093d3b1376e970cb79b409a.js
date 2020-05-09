f1e265eade72cb19c4bb6e336fbbe6ce14fbb8d1
Abdallah Ali <amaali1991@gmail.com>
2020-05-09 17:32:56 +0200

    Updates

---
+++ precache-manifest.bd6c9a47e093d3b1376e970cb79b409a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f93c2419267338b3bb78513864c79ca1",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b8620b2ccac0af332c33",
+    "url": "/monsters-rolodex/static/css/main.d1c96430.chunk.css"
+  },
+  {
+    "revision": "cefddf9fe8af0437aeaf",
+    "url": "/monsters-rolodex/static/js/2.19ef4e27.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.19ef4e27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8620b2ccac0af332c33",
+    "url": "/monsters-rolodex/static/js/main.be55d987.chunk.js"
+  },
+  {
+    "revision": "8f73a064cdcd36d959fd",
+    "url": "/monsters-rolodex/static/js/runtime-main.b2a3871a.js"
+  }
+]);
